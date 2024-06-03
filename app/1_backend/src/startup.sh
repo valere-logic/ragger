cb17b9143f0724141eb75315194aa8fc5373a201
Casirocha <casimiro@valere.io>
2024-06-03 09:39:04 -0500

    RAG-24 Removing Unnecesary Files

--- app/1_backend/src/startup.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-# TODO: Avoid unnecessarily generating server keys if use_insecure_channel is set to true.
-
-cd /app/src && gunicorn -k uvicorn.workers.UvicornWorker app:app -c ../config/prod.py
